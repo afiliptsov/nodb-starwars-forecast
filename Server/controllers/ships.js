e1f852333e10c1a3a3b37fcaf62f4b05046ad5cf
afiliptsov <filiptsovanton@gmail.com>
2018-05-11 18:04:12 -0500

    Add Ship component added

--- Server/controllers/ships.js
+++ Server/controllers/ships.js
@@ -6,19 +6,16 @@ let maxSpeed = 0;
 let crewMembers = 0;
 let cargo = 0;
 
-module.exports={
-    addShip:(req,res,next)=>{
-        req.body.id = id;
-        shipList.push(req.body);
-        id++;
-        res.status(200).send(shipList);
-    },
-    getShips: (req,res)=>{
-        res.status(200).send(shipList);
-    }
-
-
-
-
-
-}
+module.exports = {
+  addShip: (req, res, next) => {
+    console.log(req.body);
+    req.body.shipObject.id = id;
+    shipList.push(req.body.shipObject);
+    console.log(shipList);
+    id++;
+    res.status(200).send(shipList);
+  },
+  getShips: (req, res) => {
+    res.status(200).send(shipList);
+  }
+};
--- src/components/CreateAShip/CreateAShip.js
+++ src/components/CreateAShip/CreateAShip.js
@@ -1,63 +1,111 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 
 import axios from "axios";
 
-export default class CreateAShip extends Component{
-constructor(){
+export default class CreateAShip extends Component {
+  constructor() {
     super();
-    this.state = ({
-        shipArray: [
-            {
-                name: "fffds",
-                maxSpeed: 44,
-                crew: 1,
-                cargoCapacity: 455,
-                id: 0
-            }
-        ]
-
-    });
+    this.state = {
+      shipObj: {},
+      name: "",
+      maxSpeed: "",
+      crew: "",
+      cargoCapacity: "",
+      shipArray: []
+    };
 
     this.componentDidMount = this.componentDidMount.bind(this);
     this.createAShip = this.createAShip.bind(this);
+  }
 
-}
+  onChangeHandler(e, key) {
+    // console.log(this.state.shipArray);
+    // let shipArrayCopy = this.state.shipArray.slice();
+    // console.log(shipArrayCopy);
+    // shipArrayCopy[key] = e.target.value;
+    let shipObj = {};
+    shipObj[key] = e.target.value;
+    this.setState(shipObj);
+  }
 
-componentDidMount(){
+  componentDidMount() {
+    console.log("HIT");
     axios.get("http://localhost:3001/api/ships").then(response => {
-        this.setState({
-            shipArray: response.data
-        })
-    })
-}
-    createAShip(name, maxSpeed, crew, cargoCapacity){
-    axios.post("http://localhost:3001/api/ships", { name,maxSpeed,crew,cargoCapacity}).then(response=>{
+      this.setState({
+        shipArray: response.data
+      });
+    });
+    console.log("COMPONENT MOUNT");
+  }
+  createAShip() {
+    // const { shipArray } = this.state;
+    let shipObject = {
+      name: this.state.name,
+      maxSpeed: this.state.maxSpeed,
+      crew: this.state.crew,
+      cargoCapacity: this.state.cargoCapacity
+    };
+    axios
+      .post("http://localhost:3001/api/ships", { shipObject })
+      .then(response => {
         console.log(response);
         this.setState({
-            shipArray: response.data
-        })
-    })
-}
+          shipArray: response.data,
+          name: "",
+          crew: "",
+          cargoCapacity: "",
+          maxSpeed: ""
+        });
+      });
+  }
 
-    render(){
-        console.log(this.state.shipArray);
-        return (
-        <div className="create-ship-class">
-        <button onClick={()=>this.createAShip("name",23,5,10,3)}></button>
-            <input type="Name" ></input>
-            <input type="Max speed"></input>
-            <input type="Crew"></input>
-            <input type="Cargo Cap"></input>
+  getShip() {
+    axios.get("http://localhost:3001/api/ships").then(response => {
+      console.log(response);
+      this.setState({
+        shipArray: response.data[0]
+      });
+    });
+  }
 
-        
-        
-        
+  render() {
+    let mapShipArray = this.state.shipArray.map((element, i) => {
+      return (
+        <div key={i}>
+          <p>{element.name}</p>
+          <p>{element.maxSpeed}</p>
+          <p>{element.crew}</p>
+          <p>{element.cargoCapacity}</p>
         </div>
-        )
+      );
+    });
+    console.log(this.state);
+    return (
+      <div className="create-ship-class">
+        <div className="ship-input-box">
+          <button onClick={() => this.createAShip()} />
+          <label>{this.state.name}</label>
+          <input
+            value={this.state.name}
+            onChange={e => this.onChangeHandler(e, "name")}
+          />
+          <input
+            value={this.state.maxSpeed}
+            onChange={e => this.onChangeHandler(e, "maxSpeed")}
+          />
+          <input
+            value={this.state.crew}
+            onChange={e => this.onChangeHandler(e, "crew")}
+          />
+          <input
+            value={this.state.cargoCapacity}
+            onChange={e => this.onChangeHandler(e, "cargoCapacity")}
+          />
+        </div>
 
-    }
-
-
-
-
+        <div className="ship-create-result" />
+        {mapShipArray}
+      </div>
+    );
+  }
 }
